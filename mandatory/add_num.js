55307d8d2bb96bd9a4156e74bc3cabcce992672e
altom <altomhussian2001@yahoo.com>
2024-05-31 21:32:31 +0100

    adds extra deply script on ci/cd

--- mandatory/add_num.js
+++ mandatory/add_num.js
@@ -1,5 +1,8 @@
 const add = (...numbers) => numbers.reduce((total, num) => total + num, 0);
 
-test("convertToUSD function works for £32", () => {
+test("Adds two numbers", () => {
   expect(add(2, 2)).toEqual(4);
+});
+test("Adds two numbers", () => {
+  expect(add(2, 3)).toEqual(5);
 });
